4ca3c001ee81e193d067958332e2d79671c26532
Clelson Salles Rodrigues <clelson.salles@gmail.com>
2023-10-31 16:51:17 -0300

    Update plugincti_hook_listener.rb

--- lib/plugincti_hook_listener.rb
+++ lib/plugincti_hook_listener.rb
@@ -1,3 +1,3 @@
 class PluginctiHookListener < Redmine::Hook::ViewListener
-  render_on :view_layouts_base_html_head, partial: "plugincti/base_html" 
+  render_on :view_layouts_base_html_head, partial: "cti_plugin/base_html_head" 
 end
