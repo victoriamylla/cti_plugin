8491b6182c9027bee6271ebba80165e78b9a9467
Clelson Salles Rodrigues <clelson.salles@gmail.com>
2023-11-20 14:36:45 -0300

    Update plugincti_hook_listener.rb
    Alterando plugincti para inserção de footer

--- lib/plugincti_hook_listener.rb
+++ lib/plugincti_hook_listener.rb
@@ -1,3 +1,4 @@
 class PluginctiHookListener < Redmine::Hook::ViewListener
   render_on :view_layouts_base_html_head, partial: "cti_plugin/base_html" 
+  render_on :view_layouts_base_body_bottom, partial: "cti_plugin/footer"
 end
